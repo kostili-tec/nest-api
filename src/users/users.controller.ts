de39e7f02db505c8aeb60f01512d614c035c9d05
kostili-tec <weh4teyou@gmail.com>
2023-07-24 13:25:07 +0300

    feat: add swagger & create docs

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,26 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger/dist';
 import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './users.model';
 import { UsersService } from './users.service';
 
+@ApiTags('Пользователи')
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
-  @Post()
-  create(@Body() userDto: CreateUserDto) {
-    return this.usersService.createUser(userDto);
-  }
-
+  @ApiOperation({ summary: 'Получить всех пользователей' })
+  @ApiResponse({ status: 200, type: [User] })
   @Get()
   getAll() {
     return this.usersService.getAllUsers();
+  }
+
+  @ApiOperation({ summary: 'Создание пользователя' })
+  @ApiResponse({ status: 200, type: User })
+  @Post()
+  create(@Body() userDto: CreateUserDto) {
+    return this.usersService.createUser(userDto);
   }
 }
--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger/dist';
 import { Column, DataType, Model, Table } from 'sequelize-typescript';
 
 interface UserCreationAttr {
@@ -7,6 +8,7 @@ interface UserCreationAttr {
 
 @Table({ tableName: 'users' })
 export class User extends Model<User, UserCreationAttr> {
+  @ApiProperty({ example: '1', description: 'Уникальный id' })
   @Column({
     type: DataType.INTEGER,
     unique: true,
@@ -14,13 +16,17 @@ export class User extends Model<User, UserCreationAttr> {
     primaryKey: true,
   })
   id: number;
-
+  @ApiProperty({ example: 'user@mail.ru', description: 'Почтовый адрес' })
   @Column({ type: DataType.STRING, unique: true, allowNull: false })
   email: string;
+  @ApiProperty({ example: '12345678', description: 'Пароль' })
   @Column({ type: DataType.STRING, allowNull: false })
   password: string;
+  @ApiProperty({ example: 'true', description: 'Забанен или нет' })
   @Column({ type: DataType.BOOLEAN, defaultValue: false })
   banned: boolean;
+
+  @ApiProperty({ example: 'Ретард', description: 'Причина бана' })
   @Column({ type: DataType.STRING, allowNull: true })
   banReason: string;
 }
